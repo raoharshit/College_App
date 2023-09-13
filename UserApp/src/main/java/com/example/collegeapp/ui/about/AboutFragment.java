c2c26397524502417d13743c96fc5c2a9c926785
raoharshit <8708440748y@gmail.com>
2023-09-13 22:54:26 +0530

    about fragment's ui is enhanced

--- UserApp/src/main/java/com/example/collegeapp/ui/about/AboutFragment.java
+++ UserApp/src/main/java/com/example/collegeapp/ui/about/AboutFragment.java
@@ -32,13 +32,27 @@ public class AboutFragment extends Fragment {
 
 
         list = new ArrayList<>();
-        list.add(new BranchModel(R.drawable.ic_comp,"Computer Science & Engineering",getResources().getString(R.string.about_cse)));
-        list.add(new BranchModel(R.drawable.ic_mech,"Mechanical Engineering",getResources().getString(R.string.about_mech)));
+        list.add(new BranchModel(R.drawable.ic_comp,getString(R.string.cse),getResources().getString(R.string.about_cse)));
+        list.add(new BranchModel(R.drawable.ic_ece,getString(R.string.ece),getResources().getString(R.string.about_ece)));
+        list.add(new BranchModel(R.drawable.ic_elec,getString(R.string.elec),getResources().getString(R.string.about_elec)));
+        list.add(new BranchModel(R.drawable.ic_mech,getString(R.string.mech),getResources().getString(R.string.about_mech)));
+        list.add(new BranchModel(R.drawable.ic_civil,getString(R.string.civil),getResources().getString(R.string.about_civil)));
+        list.add(new BranchModel(R.drawable.ic_biomed,getString(R.string.biomed),getResources().getString(R.string.about_biomed)));
+        list.add(new BranchModel(R.drawable.ic_chemical,getString(R.string.chemical),getResources().getString(R.string.about_chemical)));
+        list.add(new BranchModel(R.drawable.ic_archi,getString(R.string.archi),getResources().getString(R.string.about_archi)));
+        list.add(new BranchModel(R.drawable.ic_chemistry,getString(R.string.chem),getResources().getString(R.string.about_chemistry)));
+        list.add(new BranchModel(R.drawable.ic_physics,getString(R.string.physics),getResources().getString(R.string.about_physics)));
+        list.add(new BranchModel(R.drawable.ic_maths,getString(R.string.maths),getResources().getString(R.string.about_maths)));
+        list.add(new BranchModel(R.drawable.ic_biotech,getString(R.string.biotech),getResources().getString(R.string.about_biotech)));
+        list.add(new BranchModel(R.drawable.ic_environment,getString(R.string.cease),getResources().getString(R.string.about_ceees)));
+        list.add(new BranchModel(R.drawable.ic_management,getString(R.string.management),getResources().getString(R.string.about_management)));
+        list.add(new BranchModel(R.drawable.ic_management,getString(R.string.humanities),getResources().getString(R.string.about_humanities)));
 
         adapter = new BranchAdapter(getContext(),list);
 
         viewPager = view.findViewById(R.id.viewPager);
         viewPager.setAdapter(adapter);
+        viewPager.setPageMargin(getResources().getDimensionPixelOffset(R.dimen.viewpager_margin));
 
 
         ImageView imageView = view.findViewById(R.id.college_image);
